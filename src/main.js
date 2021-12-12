0a02d3c312a68b21b81d6639ada1a9f3ebf00165
Clementine <406584573@qq.com>
2021-12-12 21:47:05 +0800

    html-webpack-plugin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import msg from './bar'
+import './css/style.css'
 
 console.log(msg);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,27 @@ module.exports = {
   output: {
     path: path.join(__dirname, './dist/'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
 }
